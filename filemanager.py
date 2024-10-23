09f52211fd71ea11f7d902cbd74d64f8dc00469d
nina296 <yataghanethanina@gmail.com>
2024-10-23 10:59:37 +0200

    Filemanager: write & count + log.txt

--- filemanager.py
+++ filemanager.py
@@ -10,4 +10,18 @@ class FileManager:
             print("Contenu du fichier :")
             print(content)
 
+    def write_to_file(self, data):
+        with open(self.file_path, 'a') as file:
+            file.write(data + '\n') 
+            print("Les données ont été ajoutées à la fin du fichier.")
+            
+    def count_lines(self):
+        try:
+            with open(self.file_path, 'r') as file:
+                nbr_lignes = file.readlines()  
+                return len(nbr_lignes)  
+        except FileNotFoundError:
+            print(f"Le fichier {self.file_path} n'a pas été trouvé.")
+            return 0
+
 
