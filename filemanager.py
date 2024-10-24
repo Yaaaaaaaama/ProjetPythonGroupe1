50e27a209bc6825d976e56f91c7893fdb07da35d
Yaaaaaaaama <159944838+Yaaaaaaaama@users.noreply.github.com>
2024-10-24 12:04:41 +0200

    Correction 2

--- filemanager.py
+++ filemanager.py
@@ -28,9 +28,8 @@ class FileManager:
             return 0
         except Exception as e:
             print(f"Une erreur s'est produite : {e}")
-<<<<<<< HEAD
-=======
-        
+        except Exception as e:
+            print(f"Une erreur s'est produite lors de la recherche du mot-clé : {e}")
     def search_keyword(self, keyword):
         try:
             with open(self.file_path, 'r') as file:
@@ -44,8 +43,7 @@ class FileManager:
                     print(f"Le mot-clé '{keyword}' n'a pas été trouvé.")
         except FileNotFoundError:
             print(f"Le fichier {self.file_path} est introuvable.")
->>>>>>> 3130d74850cc4199f34489036f8e405b86e409c0
         except Exception as e:
             print(f"Une erreur s'est produite lors de la recherche du mot-clé : {e}")
-
+ 
 
